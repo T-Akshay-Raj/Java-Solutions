7206c402cbf17236f3fb36b12466eefd9b01fd2a
T-Akshay-Raj <161284052+T-Akshay-Raj@users.noreply.github.com>
2024-04-19 20:37:30 +0530

    Update SwitchStatement.java

--- Java 14/SwitchStatement.java
+++ Java 14/SwitchStatement.java
@@ -5,6 +5,9 @@ public class SwitchStatement {
 
     System.out.println(isFeedingTime(10));//false
     System.out.println(isFeedingTime(21));//true
+
+    System.out.println(isFeedingTime1(10));//false
+    System.out.println(isFeedingTime1(21));//true
   }
 
   //switch statement example as per java 14 allows grouping case separated by "," and use "->" to overcome break statement
@@ -15,5 +18,19 @@ public class SwitchStatement {
       default -> isFeedingTime = false;
     }
     return isFeedingTime;
+  }
+
+  //switch statement example as per java 14 allows grouping case separated by "," and use "->" to overcome break statement and yield for multi statements
+  public static boolean isFeedingTime1(int hour) {
+    boolean isFeedingTime =
+        switch (hour) {
+          case 2, 16, 21 -> true;
+          default -> {
+            System.out.println("There is still time for feeding!!");
+            yield false;
+          }
+        };
+    return isFeedingTime;
+
   }
 }
